9d1a663adea12aa563ecdedba3c7c6db3f48511f
ruchi-sharma186 <ruchisharmasharma216@gmail.com>
2025-03-21 19:34:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log("hello world");
+
+//arithmetic operators
+let num1 = 45;
+
+let num2 = 15;
+console.log(num1 + num2);
+
+//comparision operators
+let num3 =10;
+ let num4 = 20;
+ console.log(num3 == num4);  
+ console.log(num3 === num4);  // means that equal value  and data type
+ console.log(num3 !== num4);  // != it means if any one condition is true the give output is true
+   
+ console.log(num1 > num2);
+ console.log(num1 < num2);
+ console.log(num1 <=  num2)
+ console.log(num1 >=  num2)
+
+
+ let a = 80;
+ a -= +15;
+ console.log(a)
+
+ let b = 80;
+ b = -15;
+ console.log(b)
+
+
+let c = 80;
+ a %= 15;
+ console.log(c)
+
+ //increment operators
+ let d= 80;
+ //d++;
+ //console.log(d)
+ console.log(d++,++d,d, d, d, d++, d++,d++, d, ++d, ++d, ++d, d++,d++)
+//80 82 82 82 82 82 83 84 85 86 87 88 88 89
+
+ let e=43;
+ console.log(e, e++, --e, e--, e++, e, e++, ++e, --e, --e, e++)
+ //43 43 43 43 42 43 43 45 44 43 43
+
